2db6ec1d27645936715422ebff496c77ddb7849f
Naomi Dushay <ndushay@stanford.edu>
2014-02-18 17:40:55 -0800

    add format to fields checked for all gdor records

--- spec/all_gdor_records_spec.rb
+++ spec/all_gdor_records_spec.rb
@@ -18,6 +18,10 @@ describe "All GDOR records" do
     resp = solr_resp_doc_ids_only({'fq'=> ["-collection:sirsi", "-druid:*"]})
     resp.should_not have_documents
   end
+  it "should have a format" do
+    resp = solr_resp_doc_ids_only({'fq'=> ["-collection:sirsi", "-format:*"]})
+    resp.should_not have_documents
+  end
   it "should have a sortable title" do
     resp = solr_resp_doc_ids_only({'fq'=> ["-collection:sirsi", "-title_sort:*"]})
     resp.should_not have_documents
