e066d072c542052d2ac197f17b39bb16e5646681
Laura Wilsey <lauraw15@stanford.edu>
2014-03-04 12:08:12 -0800

    broke hydrus_colls_spec.rb tests into general, UARCH and ARS categories; put tests in alpha order by collection title

--- spec/hydrus_colls_spec.rb
+++ spec/hydrus_colls_spec.rb
@@ -35,36 +35,35 @@ describe "Hydrus collections" do
 #    it_behaves_like "language", facet_query
   end
 
-  context "Folding@home" do
-    it_behaves_like 'hydrus coll', 'cj269gn0736', 4, "hp35 trajectory data", 'bd829sf1034', 3
-#    facet_query = "collection:cj269gn0736"
-#    it_behaves_like "author fields present", facet_query
-  end
-  context "Preserving Virtual Worlds" do
-    it_behaves_like 'hydrus coll', 'sn446tz2204', 8, "star raiders", 'pp060nc9006', 3
-  end
-  context "Stanford Research Data" do
-    it_behaves_like 'hydrus coll', 'md919gh6774', 5, "high angular resolution", 'yx282xq2090', 3
-  end
-  context "SUL staff publications" do
-    it_behaves_like 'hydrus coll', 'hn730ks3626', 4, "academy unbound", 'bd701dh8028', 3
-  end
-  context "Physic Undergrad Theses" do
-    it_behaves_like 'hydrus coll', 'ds247vz0452', 17, "scanning squid", 'gh325bb5942', 3
-  end
-  
   context "Big Ideas" do
     it_behaves_like 'hydrus coll', 'xf112dv1419', 74, "alix personal", 'mq607rm1165', 3
   end
-  
-  context "ME310 2013" do
-    it_behaves_like 'hydrus coll', 'kq629sd5182', 8, "audievolve", 'qt429km6702', 3
+  context "Folding@home" do
+    it_behaves_like 'hydrus coll', 'cj269gn0736', 4, "hp35 trajectory data", 'bd829sf1034', 3
   end
   context "engineering physics undergrad theses" do
     it_behaves_like 'hydrus coll', 'xv924ks7647', 3, "deduceit", 'bg823wn2892', 3
   end
   context "engineering undergrad theses" do
     it_behaves_like 'hydrus coll', 'jg722zc0626', 30, "uclinux", 'ng517gq2855', 3
+  end
+  context "ME310 2013" do
+    it_behaves_like 'hydrus coll', 'kq629sd5182', 8, "audievolve", 'qt429km6702', 3
+  end
+  context "Physics Undergrad Theses" do
+    it_behaves_like 'hydrus coll', 'ds247vz0452', 17, "scanning squid", 'gh325bb5942', 3
+  end
+  context "Preserving Virtual Worlds" do
+    it_behaves_like 'hydrus coll', 'sn446tz2204', 8, "star raiders", 'pp060nc9006', 3
+  end
+  context "Research Datasets for Image, Video, and Multimedia Systems Group @ Stanford" do
+    it_behaves_like 'hydrus coll', 'cm018rf5314', 6, "Stanford Streaming Mobile Augmented Reality Dataset", 'ph459zk5920', 5
+  end
+  context "Stanford Research Data" do
+    it_behaves_like 'hydrus coll', 'md919gh6774', 5, "high angular resolution", 'yx282xq2090', 3
+  end
+  context "SUL staff publications" do
+    it_behaves_like 'hydrus coll', 'hn730ks3626', 4, "academy unbound", 'bd701dh8028', 3
   end
   context "Undergraduate Honors Theses, Graduate School of Education" do
     it_behaves_like 'hydrus coll', 'qs035dj7859', 4, "Civic Engagement in Anakbayan", 'jw598xm2819', 3
@@ -73,56 +72,48 @@ describe "Hydrus collections" do
     it_behaves_like 'hydrus coll', 'qd500xn1572', 4, "Asynchronous broadband signals", 'hj582pj3902', 3
   end
   
-  context "Stanford Artificial Intelligence Laboratory Records" do
-    it_behaves_like 'hydrus coll', 'jb056mm1304', 3, "35th reunion 2009", 'pb496bf3201', 5
-  end
   
-  context "Research Datasets for Image, Video, and Multimedia Systems Group @ Stanford" do
-    it_behaves_like 'hydrus coll', 'cm018rf5314', 6, "Stanford Streaming Mobile Augmented Reality Dataset", 'ph459zk5920', 5
-  end
+  # University Archives Hydrus Collections
   
-  context "Stanford University Film Collection" do
-    it_behaves_like 'hydrus coll', 'tm335zd3912', 1, "stanford university film collection films 1937", 'zv044zr8616', 10
+  context "David Starr Jordan Papers" do
+    it_behaves_like 'hydrus coll', 'jy460rb9016', 2, "david starr jordan business correspondence", 'sw878zc4588', 10
   end
-  
   context "H. Bruce Franklin Collection" do
     it_behaves_like 'hydrus coll', 'pn278bq2224', 11, "Advisory Board decision in the matter of professor H. Bruce Franklin", 'cm175mf2096', 5
   end
+  context "Jane Lathrop Stanford Papers" do
+    it_behaves_like 'hydrus coll', 'hs468px3695', 3, "death and estate papers", 'fk544dk9538', 10
+  end
+  context "Leland Stanford Papers" do
+    it_behaves_like 'hydrus coll', 'zx692xz8270', 3, "leland stanford legal papers", 'kh510mt0132', 10
+  end
+  context "Martin Luther King at Stanford" do
+    it_behaves_like 'hydrus coll', 'yt337pb3236', 3, "martin luther king at stanford", 'dn923nh8281', 10
+  end
+  context "Stanford Artificial Intelligence Laboratory Records" do
+    it_behaves_like 'hydrus coll', 'jb056mm1304', 3, "35th reunion 2009", 'pb496bf3201', 5
+  end
+  context "The Stanford Flipside" do
+    it_behaves_like 'hydrus coll', 'dq441rn2614', 3, "stanford flipside 2008", 'bv723rv4628', 10
+  end
+  context "Stanford Student Letters and Memoirs" do
+    it_behaves_like 'hydrus coll', 'mt423yd8582', 1, "letters and memoirs", 'jv222bg0652', 10
+  end
+  context "Stanford University Commencement Collection" do
+    it_behaves_like 'hydrus coll', 'nz928tt4938', 3, "commencement addresses", 'mp840zw9344', 10
+  end
+  context "Stanford University Film Collection" do
+    it_behaves_like 'hydrus coll', 'tm335zd3912', 1, "stanford university film collection films 1937", 'zv044zr8616', 10
+  end
+  context "Stanford University Video Collection" do
+    it_behaves_like 'hydrus coll', 'gn946cw3927', 1, "stanford university videos 1939", 'ts408hz9199', 20
+  end
+  
+  # Archive of Recorded Sound Hydrus Collections
   
   context "Rigler-Deutsch Computer Tapes" do
     it_behaves_like 'hydrus coll', 'jd276dz9994', 3, "Rigler & Deutsch Record Index", 'cr661vw3932', 10
   end
   
-  context "Jane Lathrop Stanford Papers" do
-    it_behaves_like 'hydrus coll', 'hs468px3695', 3, "death and estate papers", 'fk544dk9538', 10
-  end
-  
-  context "Stanford University Video Collection" do
-    it_behaves_like 'hydrus coll', 'gn946cw3927', 1, "stanford university videos 1939", 'ts408hz9199', 20
-  end
-  
-  context "Stanford Student Letters and Memoirs" do
-    it_behaves_like 'hydrus coll', 'mt423yd8582', 1, "letters and memoirs", 'jv222bg0652', 10
-  end
-  
-  context "David Starr Jordan Papers" do
-    it_behaves_like 'hydrus coll', 'jy460rb9016', 2, "david starr jordan business correspondence", 'sw878zc4588', 10
-  end
-  
-  context "The Stanford Flipside" do
-    it_behaves_like 'hydrus coll', 'dq441rn2614', 3, "stanford flipside 2008", 'bv723rv4628', 10
-  end
-  
-  context "Leland Stanford Papers" do
-    it_behaves_like 'hydrus coll', 'zx692xz8270', 3, "leland stanford legal papers", 'kh510mt0132', 10
-  end
-  
-  context "Stanford University Commencement Collection" do
-    it_behaves_like 'hydrus coll', 'nz928tt4938', 3, "commencement addresses", 'mp840zw9344', 10
-  end
-  
-  context "Martin Luther King at Stanford" do
-    it_behaves_like 'hydrus coll', 'yt337pb3236', 3, "martin luther king at stanford", 'dn923nh8281', 10
-  end
   
 end
