c9fbbd636338c1c825ae5faeb3c42f0fc374fd22
Laura Wilsey <lauraw15@stanford.edu>
2014-03-04 12:27:27 -0800

    put collections in alpha order in file non_hydrus_colls_spec.rb

--- spec/non_hydrus_colls_spec.rb
+++ spec/non_hydrus_colls_spec.rb
@@ -4,6 +4,17 @@ describe "DOR Digital Collections" do
 
   context "merged coll records" do
     # id of collection record is ckey from Sirsi, not druid from DOR
+    
+    context "Francis E. Stafford photographs" do
+      ckey = '9615156'
+      it_behaves_like "all items in collection", ckey, 5
+      it_behaves_like "DOR collection object", ckey, 'yg867hg1375'
+      it_behaves_like "DOR item objects", "seventh day adventist church missionaries", ['nz353cp1092'], 10, ckey
+      facet_query = "collection:#{ckey}"
+      it_behaves_like "date fields present", facet_query
+#      it_behaves_like "author fields present", facet_query
+#      it_behaves_like "language", facet_query
+    end
     context "Kolb" do
       ckey = '4084372'
       it_behaves_like "all items in collection", ckey, 1402
@@ -26,30 +37,12 @@ describe "DOR Digital Collections" do
 #      it_behaves_like "author fields present", facet_query
 #      it_behaves_like "language", facet_query
     end
-    context "Francis E. Stafford photographs" do
-      ckey = '9615156'
-      it_behaves_like "all items in collection", ckey, 5
-      it_behaves_like "DOR collection object", ckey, 'yg867hg1375'
-      it_behaves_like "DOR item objects", "seventh day adventist church missionaries", ['nz353cp1092'], 10, ckey
-      facet_query = "collection:#{ckey}"
-      it_behaves_like "date fields present", facet_query
-#      it_behaves_like "author fields present", facet_query
-#      it_behaves_like "language", facet_query
-    end
-  end
+  end # merged coll records
+  
   
   context "no marc coll record" do
     # id of collection record in Solr is druid from DOR
-    context "Walters Manuscripts" do
-      coll_id = 'ww121ss5000'
-      it_behaves_like "all items in collection", coll_id, 298
-      it_behaves_like "DOR collection object", coll_id, coll_id
-      it_behaves_like "DOR item objects", "walters brasses", ['cn006dx2288'], 3, coll_id
-      facet_query = "collection:#{coll_id}"
-      it_behaves_like "date fields present", facet_query
-#      it_behaves_like "author fields present", facet_query
-#      it_behaves_like "language", facet_query
-    end
+  
     context "Classics Papyri" do
       coll_id = 'jr022nf7673'
       it_behaves_like "all items in collection", coll_id, 44
@@ -77,6 +70,16 @@ describe "DOR Digital Collections" do
       it_behaves_like "all items in collection", coll_id, 13
       it_behaves_like "DOR collection object", coll_id, coll_id
       it_behaves_like "DOR item objects", "melita", ['zz360bw3691'], 10, coll_id
+      facet_query = "collection:#{coll_id}"
+      it_behaves_like "date fields present", facet_query
+#      it_behaves_like "author fields present", facet_query
+#      it_behaves_like "language", facet_query
+    end
+    context "Walters Manuscripts" do
+      coll_id = 'ww121ss5000'
+      it_behaves_like "all items in collection", coll_id, 298
+      it_behaves_like "DOR collection object", coll_id, coll_id
+      it_behaves_like "DOR item objects", "walters brasses", ['cn006dx2288'], 3, coll_id
       facet_query = "collection:#{coll_id}"
       it_behaves_like "date fields present", facet_query
 #      it_behaves_like "author fields present", facet_query
