018bc48ff6c3003d13d610248c4f956b77628d71
Naomi Dushay <ndushay@stanford.edu>
2014-06-13 01:04:10 -0700

    fix rigler item search (remove ampersand from query terms)

--- spec/hydrus_colls_spec.rb
+++ spec/hydrus_colls_spec.rb
@@ -148,7 +148,7 @@ describe "Hydrus collections" do
   # Archive of Recorded Sound Hydrus Collections
   
   context "Rigler-Deutsch Computer Tapes" do
-    it_behaves_like 'hydrus coll', 'jd276dz9994', 5, "file", ["Book", "Computer File", "Manuscript/Archive"], nil, "Rigler & Deutsch Record Index", 'cr661vw3932', 10
+    it_behaves_like 'hydrus coll', 'jd276dz9994', 5, "file", ["Book", "Computer File", "Manuscript/Archive"], nil, "Rigler Deutsch Record Index", 'cr661vw3932', 10
   end
   
 end
